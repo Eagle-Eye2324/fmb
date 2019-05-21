3f51f9b304c9e7ea038f2f67f2068a26fe6b7161
Eagle-Eye2324 <rmunro2324@gmail.com>
2019-05-20 17:06:30 -0700

    Initial commit from local

---
+++ Flight Management Bot/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using Newtonsoft.Json;
+
+namespace FlightManagementBot
+{
+    class Program
+    {
+        static DiscordClient discord;
+        static CommandsNextModule commands;
+
+        static void Main(string[] args)
+        {
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            // first, let's load our configuration file
+            var json = "";
+            using (var fs = File.OpenRead("config.json"))
+            using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+                json = await sr.ReadToEndAsync();
+
+            // next, let's load the values from that file
+            // to our client's configuration
+            var cfgjson = JsonConvert.DeserializeObject<ConfigJson>(json);
+
+            discord = new DiscordClient(new DiscordConfiguration
+            {
+                Token = cfgjson.Token, 
+                TokenType = TokenType.Bot,
+
+                AutoReconnect = true,
+                UseInternalLogHandler = true,
+                LogLevel = LogLevel.Debug
+            });
+
+            discord.MessageCreated += async e =>
+            {
+                if (e.Message.Content.ToLower().StartsWith("ping"))
+                    await e.Message.RespondAsync("pong!");
+            };
+
+            commands = discord.UseCommandsNext(new CommandsNextConfiguration
+            {
+                StringPrefix = ";;"
+            });
+
+            commands.RegisterCommands<FMBCommands>();
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+
+    public struct ConfigJson
+    {
+        [JsonProperty("token")]
+        public string Token { get; private set; }
+
+        [JsonProperty("prefix")]
+        public string CommandPrefix { get; private set; }
+    }
+}
